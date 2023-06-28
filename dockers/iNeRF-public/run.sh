b44d8b02b9558178bfbf0f1d2a4f9dcbdeb8594a
DavidBoja <david.bojanic@hotmail.com>
2023-06-28 20:51:03 +0000

    Fixed iNeRF run.sh script

--- dockers/iNeRF-public/run.sh
+++ dockers/iNeRF-public/run.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
-CODE_DIR_PATH=$1
-DATA_DIR_PATH=$2
 
+if [ $# -eq 0 ]
+  then
+    echo "You need to supply CODE and (optionally) DATA paths as specified in the README"
+    exit 1
+fi
+
+CODE_DIR_PATH=$1
+
+if [ -z "$2" ]
+  then
+    # run docker image without data path mounting
+    docker run --gpus all --name inerf-container -t -v $CODE_DIR_PATH/:/iNeRF-public inerf
+    exit 1
+fi
+
+DATA_DIR_PATH=$2
 docker run --gpus all --name inerf-container -t -v $CODE_DIR_PATH/:/iNeRF-public -v $DATA_DIR_PATH/:/data inerf
