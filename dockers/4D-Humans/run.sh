4bbc7f4daa0c8d21266f6676ddc823f3a4c1cbe5
DavidBoja <david.bojanic@hotmail.com>
2023-06-29 20:48:24 +0000

    Added 4D-Humans implementation.

---
+++ dockers/4D-Humans/run.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [ $# -eq 0 ]
+  then
+    echo "You need to supply CODE and (optionally) DATA paths as specified in the README"
+    exit 1
+fi
+
+CODE_DIR_PATH=$1
+
+if [ -z "$2" ]
+  then
+    # run docker image without data path mounting
+    docker run --gpus all --shm-size=8gb --name four-d-humans-container -t -v $CODE_DIR_PATH/:/4D-Humans four-d-humans
+    exit 1
+fi
+
+DATA_DIR_PATH=$2
+docker run --gpus all --shm-size=8gb --name four-d-humans-container -t -v $CODE_DIR_PATH/:/4D-Humans -v $DATA_DIR_PATH/:/data four-d-humans
